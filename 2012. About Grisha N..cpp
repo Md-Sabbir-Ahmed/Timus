fca59c51944337c7f0d4b29a4c05fcfc9e1e51b3
Sabbir <sabbir106706@gmail.com>
2020-05-26 02:01:43 +0600

    p4 using cpp

---
+++ 2012. About Grisha N..cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    int f,r;
+    cin>>f;
+    r=(12-f)*45;
+    if(r<=240)
+    {
+    	cout<<"YES\n";
+    }
+    else
+    {
+    	cout<<"NO\n";
+    }
+    return 0;
+}
+
