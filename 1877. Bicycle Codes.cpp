33ce53b483d18c229359f754eed6d6edceac60d3
Sabbir <sabbir106706@gmail.com>
2020-05-26 02:15:27 +0600

    p5 using cpp

---
+++ 1877. Bicycle Codes.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    int a,b;
+    cin>>a;
+    cin>>b;
+    if((a%2==0)||(b%2!=0))
+    {
+    	cout<<"yes";
+    }
+    else
+    {
+    	cout<<"no";
+    }
+    
+    return 0;
+}
+
